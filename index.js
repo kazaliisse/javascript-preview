dc97798bea70abb229929f7d504c9be462c20fc2
noor <noorhafowbare@gmail.com>
2024-06-10 16:07:09 +0300

    variables

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 console.log('hello world')
 console.log('Noor Isse')
+
+// variables
+name = 'noor kazali';
+console.log(name);
+
+sentences = 'how are you today, nice to see you'
+console.log(sentences)
+
+
+
+
