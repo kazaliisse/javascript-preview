32860de392de5a99304b0f33b37a2c2e08983167
noor <noorhafowbare@gmail.com>
2024-06-10 16:26:31 +0300

    operators

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ console.log(name);
 sentences = 'how are you today, nice to see you'
 console.log(sentences)
 
-
+// operators
+food = 20
+tipPercentage = 0.2
+tipAmount = food * tipPercentage
+console.log(tipAmount)
 
 
